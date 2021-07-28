a64bc4792858d7fa4040137f001d395afa5bd0e4
Amir Kasumovic <amir.kasumovic@live.com>
2021-07-28 14:01:12 +0200

    navbar colors

--- view/src/components/navbar/Navbar.tsx
+++ view/src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import {GlobalState} from '../../GlobalState'
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
@@ -13,11 +14,40 @@ export const Navbar: React.FC = ({}: Props) => {
     const value = React.useContext(GlobalState);
 
     return (
-        <header>
-            <MenuIcon />
-            <CloseIcon />
-            <ShoppingCartIcon />
-            {/* {value} */}
-        </header>
+        <nav>
+            <div className="menu">
+                <MenuIcon className="menuIcon"/>
+            </div>
+            
+
+            <div className="logo">
+                <Link className="logo" to="/">RiboSport</Link>
+            </div>
+            <div className="menuItems">
+                <ul>
+                    <li>
+                        <Link className="navLink" to="/">Pocetna</Link>
+                    </li>
+                    <li>
+                        <Link className="navLink" to="/products">Kupovina</Link>
+                    </li>
+                    <li>
+                        <Link className="navLink" to="/login">Prijava</Link>
+                    </li>
+                    <li>
+                        <Link className="navLink" to="/register">Registracija</Link>
+                    </li>
+                    <li>
+                        <span>0</span>
+                        <Link className="navLink" to="/cart">
+                            <ShoppingCartIcon className="closeIcon"/>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="closeBtn">
+                    <CloseIcon className="cartIcon"/>
+                </div>
+            </div>
+        </nav>
     )
 }
