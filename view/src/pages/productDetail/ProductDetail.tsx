7e6fe995d5fccbbed8484d59cd9fd26873caae9e
Amir Kasumovic <amir.kasumovic@live.com>
2021-07-29 15:15:44 +0200

    product details info pull

--- view/src/pages/productDetail/ProductDetail.tsx
+++ view/src/pages/productDetail/ProductDetail.tsx
@@ -1,15 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import {useParams} from 'react-router-dom'
+import {GlobalState} from '../../GlobalState'
+import {IState as IProduct} from '../../pages/products/Products'
 
 interface Props {
-    
+    id: any;
+    detailProduct: IProduct['product'];
 }
 
-export const ProductDetail: React.FC = ({}: Props) => {
+export const ProductDetail: React.FC = () => {
+    const {id}:Props['id'] = useParams();
+    const state= useContext(GlobalState);
+    const [products] = state?.productsAPI.products;
+    const [detailProduct, setDetailProduct] = useState({});
+
+    useEffect(() => {
+        if(id){
+            products.forEach((product:IProduct['product']) => {
+                if(product._id === id){
+                    setDetailProduct(product);
+                }
+            })
+        }
+    }, [id, products])
+    console.log(detailProduct);
+
     return (
-        <div>
-            PRODUCT DETAIL
+        <div className="detailProductWrapper">
+            <div className="detailProduct">
+                
+            </div>
         </div>
     )
 }
-
-
