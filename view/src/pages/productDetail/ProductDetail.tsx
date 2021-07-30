c72a5ad0a625d3e4b89a29417849de83b6bb6da4
Amir Kasumovic <amir.kasumovic@live.com>
2021-07-30 09:16:10 +0200

    product details structure done

--- view/src/pages/productDetail/ProductDetail.tsx
+++ view/src/pages/productDetail/ProductDetail.tsx
@@ -2,18 +2,37 @@ import React, { useState, useEffect, useContext } from 'react'
 import {useParams} from 'react-router-dom'
 import {GlobalState} from '../../GlobalState'
 import {IState as IProduct} from '../../pages/products/Products'
+import {Link} from 'react-router-dom';
 import './ProductDetail.css'
 
 interface Props {
     id: any;
-    detailProduct: IProduct['product'];
+    // detailProduct: IProduct['product'];
+    detailProduct: {
+        category: string;
+        checked: boolean;
+        content: string;
+        createdAt: any;
+        description: string;
+        images: {
+            public_id: string; 
+            url: string;}
+        price: number;
+        product_id: string;
+        sold: number;
+        title: string;
+        updatedAt: any;
+        __v: any;
+        _id: number;
+    } | {} | any;
+    setDetailProduct: React.Dispatch<React.SetStateAction<{}>>;
 }
 
 export const ProductDetail: React.FC = () => {
     const {id}:Props['id'] = useParams();
     const state= useContext(GlobalState);
     const [products] = state?.productsAPI.products;
-    const [detailProduct, setDetailProduct] = useState({});
+    const [detailProduct, setDetailProduct] = useState<Props['detailProduct']>({});
 
 
 
@@ -36,10 +55,21 @@ export const ProductDetail: React.FC = () => {
         <div className="detailProductWrapper">
             <div className="detailProduct">
                 <div className="detailProductLeft">
-
+                    <img src={detailProduct.images.url} alt="" className="detailProductImg" />
                 </div>
                 <div className="detailProductRight">
-                    
+                    <div className="detailProductRightTop">
+                        <h1 className="detailProductTitle">{detailProduct.title}</h1>
+                        <p className="detailProductDescription">{detailProduct.description}</p>
+                    </div>
+                    <div className="detailProductRightBottom">
+                        <p className="detailProductPrice">Cijena: {detailProduct.price}KM</p>
+                        <div className="detailProductButtons">
+                            <Link id="detailProductBuyBtn" to="#!">
+                                KUPI
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
